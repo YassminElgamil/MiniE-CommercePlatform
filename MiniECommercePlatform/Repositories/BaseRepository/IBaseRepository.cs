798bd0e4e99d49277863c842689b9aaa0a2b35c4
Yassmin Elgameel <yassmin.anwer.elgameel@gmail.com>
2025-12-18 00:53:32 +0200

    Add Generic Repository and UnitOfWork Interface

---
+++ MiniECommercePlatform/Repositories/BaseRepository/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniECommercePlatform;
+
+public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+{
+    public Task<TEntity> Create(TEntity entity);
+    public Task<TEntity> Update(TEntity entity);
+    public Task<TEntity> Delete(TEntity entity);
+    public Task<TEntity?> GetById(Guid id);
+    public Task<IQueryable<TEntity>?> Get();
+    public Task<IQueryable<TEntity>> Get(Expression<Func<TEntity, bool>> expression);
+}
---
+++ MiniECommercePlatform/UnitOfWorks/BaseUnitOfWork/IBaseUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniECommercePlatform;
+
+public interface IBaseUnitOfWork<TEntity> where TEntity : BaseEntity
+{
+    public Task<TEntity> Create(TEntity entity);
+    public Task<TEntity> Update(TEntity entity);
+    public Task Delete(TEntity entity);
+    public Task<TEntity> Read(Guid id);
+    public Task<IQueryable<TEntity>> Read();
+    public Task<IQueryable<TEntity>> Read(Expression<Func<TEntity,bool>> expression);
+}
